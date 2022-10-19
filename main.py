f8b220c15c540ccfaca7d298d57eafa99a594f7c
Ti-K <timo.klesy@bmw.de>
2022-10-19 15:16:39 +0200

    Schreiben in Datei funktioniert jetzt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: iso-8859-1 -*-
 
 import json
+import datetime
 
 
 f = open('data.json')
@@ -9,8 +10,9 @@ f = open('data.json')
 data = json.load(f)
 nextDepartures = []
 
-for i in range(5):
-    nextDepartures.append(data[i])
+for i in range(20):
+    if data[i]["plannedDepartureTime"] / 1000 + data[i]["delayInMinutes"] * 60 - datetime.datetime.now().timestamp() - 1200 >= 0:
+        nextDepartures.append(data[i])
 
 with open("output.json", "w") as outfile:
     outfile.write(json.dumps(nextDepartures))
